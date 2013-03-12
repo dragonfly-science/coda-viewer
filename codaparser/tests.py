1291b6a52bfe7a941e28b4efa676252eb78d1cd7
Christopher Knox <chris@dragonfly.co.nz>
2013-03-13 12:14:03 +1300

    test file for codaparser

---
+++ codaparser/tests.py
@@ -0,0 +1,22 @@
+from codaparser import CodaResults
+from nose.tools import ok_, eq_, assert_almost_equal
+import os
+
+bad_coda_directory = "this_is_not_a_directory"
+basedir = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+good_coda_directory = basedir + '/testdata/dolphins'
+
+
+def test_variable_list():
+    coda = CodaResults(good_coda_directory)
+    assert 'tau.eta' in coda.variables
+    
+
+def test_chain_one():
+    coda = CodaResults(good_coda_directory)
+    data = coda.get_data('lambda')
+    assert_almost_equal(data[5], 2.11997, 5)
+    assert_almost_equal(data[4999], 2.27574, 5)
+    data = coda.get_data('beta.lambda[3]')
+    assert_almost_equal(data[0], -7.92222, 5)
+    
