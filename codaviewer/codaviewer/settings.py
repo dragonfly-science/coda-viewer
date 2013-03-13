32dff395d1248c511dde389af58bb7a9ede0db6f
Christopher Knox <chris@dragonfly.co.nz>
2013-03-13 14:08:59 +1300

    Made django static files aware of brunch generated files

--- codaviewer/codaviewer/settings.py
+++ codaviewer/codaviewer/settings.py
@@ -81,7 +81,10 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    BASEDIR + '/codarender/public/',
 )
+
+print STATICFILES_DIRS
 
 # List of finder classes that know how to find static files in
 # various locations.
